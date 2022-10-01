0e07bb1add6b7b8582fb901f347ac0e1fb90e968
hariwibdj <hariwib@darmajaya.ac.id>
2022-10-01 09:32:25 +0700

    update model buku, tambah controller buku

--- app/Models/Buku.php
+++ app/Models/Buku.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Buku extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'judul',
+        'thumbnail',
+        'harga',
+    ];
 }
